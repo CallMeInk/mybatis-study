586b09b79e7c8d030177641c13aee76e45dadec6
yukai <yukai@comteck.cn>
2021-10-22 13:57:17 +0800

    study

---
+++ mybatis-01/src/main/java/com/kuang/dao/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao{
+    public List<User> getUserList() {
+        return null;
+    }
+}
---
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,25 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+
+    @Test
+    public void test(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        List<User> userList = mapper.getUserList();
+
+        for(User user : userList){
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+    }
+
+}
