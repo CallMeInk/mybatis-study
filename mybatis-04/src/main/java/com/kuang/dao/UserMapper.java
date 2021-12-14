a63c30741590f9955d9222ad5b5107edce781c67
yukai <yukai@comteck.cn>
2021-12-14 11:01:40 +0800

    分页

--- mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
@@ -3,10 +3,15 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
     List<User> getUserList();
+
+    List<User> getUserByLimit(Map<String, Integer> map);
+
+    List<User> getUserByRowBounds();
 
     User getUserById(int id);
 
